f523ce1cb3b0c47c1a5b475e54a760f2204f5870
Krunal Darji <krunal.darji@speridian.com>
2026-05-16 12:31:39 +0000

    Try fixing this error: `Runtime TypeError: Cannot read properties of nul

--- src/firebase/auth/use-user.tsx
+++ src/firebase/auth/use-user.tsx
@@ -11,6 +11,11 @@ export function useUser() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    if (!auth) {
+      setLoading(false);
+      return;
+    }
+
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
       setLoading(false);
